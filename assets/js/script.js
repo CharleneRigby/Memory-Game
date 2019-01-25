b8b7ee373e4d2ecbda0dcde17ac36d7ed61b664f
Char <CharleneLRigby@gmail.com>
2019-01-24 19:31:52 -0800

    version 2.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,8 +29,8 @@ function checkForMatch() {
 }
 
 function disableCards() {
-  firstCard.removeEventListener('touchstart', flipCard);
-  secondCard.removeEventListener('touchstart', flipCard);
+  /*firstCard.removeEventListener('touchstart', flipCard);
+  secondCard.removeEventListener('touchstart', flipCard);*/
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
   
@@ -58,5 +58,5 @@ function resetBoard() {
   });
 })();
 
-cards.forEach(card => card.addEventListener('touchstart', flipCard));
+/*cards.forEach(card => card.addEventListener('touchstart', flipCard));*/
 cards.forEach(card => card.addEventListener('click', flipCard));
